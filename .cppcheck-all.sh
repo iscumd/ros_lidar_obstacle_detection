970a3e4da5a7209a5fe1f20031a8c74f3918029c
aaron <you@example.com>
2018-05-21 15:57:32 -0400

    exclude build/ from static check

--- .cppcheck-all.sh
+++ .cppcheck-all.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 cd lidar_obstacle_detection
-cppcheck . --enable=performance,warning,performance,portability,information,missingInclude,style -i external/catch/catch.hpp -i tests/test.cpp --error-exitcode=1 --report-progress --std=c++11 -j1 -I include/ 1 > /dev/null 2>>../error.txt
+cppcheck src/ --enable=performance,warning,performance,portability,information,missingInclude,style -i --error-exitcode=1 --report-progress --std=c++11 -j1 -I include/ 1 > /dev/null 2>>../error.txt
+cppcheck tests/ --enable=performance,warning,performance,portability,information,missingInclude,style -i test.cpp -i --error-exitcode=1 --report-progress --std=c++11 -j1 -I include/ 1 > /dev/null 2>>../error.txt
 cd ..
 grep -v "Cppcheck cannot find all the include files" error.txt > error_filtered.txt
 cat error_filtered.txt
